451e172ed8a1a489aa7ab9d6004562921b4f14bb
ooppi <49905278+ooppi@users.noreply.github.com>
2021-08-21 22:21:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,4 +4,4 @@ echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 git clone https://github.com/DevOpenWRT-Router/luci-app-rebootschedule.git package/lean/luci-app-rebootschedule
 git clone https://github.com/mitsukileung/luci-app-filebrowser.git package/lean/luci-app-filebrowser
-git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
+# git clone https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus
