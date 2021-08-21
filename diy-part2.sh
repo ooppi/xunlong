7ef40c1fb4012aae5678124daecb4a6c56191669
ooppi <49905278+ooppi@users.noreply.github.com>
2021-08-21 22:21:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,7 +1,7 @@
-ZZZ="package/lean/default-settings/files/zzz-default-settings"
+# ZZZ="package/lean/default-settings/files/zzz-default-settings"
 
-sed -i 's/192.168.1.1/192.168.0.55/g' package/base-files/files/bin/config_generate             # 修改IP地址为192.168.0.55
+sed -i 's/192.168.1.1/192.168.0.30/g' package/base-files/files/bin/config_generate             # 修改IP地址为192.168.0.55
 
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile        # 选择argon为默认主题
-sed -i "s/OpenWrt /faceroo compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ         # 增加个性名字 faceroo
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                         # 设置密码为空
+# sed -i "s/OpenWrt /faceroo compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ         # 增加个性名字 faceroo
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                         # 设置密码为空
